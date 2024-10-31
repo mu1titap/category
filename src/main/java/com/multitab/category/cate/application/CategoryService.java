4f811170a5581e17bf12f24941849753c4c7e48b
kimdayday <kdh39278@gmail.com>
2024-10-31 17:12:35 +0900

    refactor: ã bot 카테고리 dto,vo,service 삭제

--- src/main/java/com/multitab/category/cate/application/CategoryService.java
+++ src/main/java/com/multitab/category/cate/application/CategoryService.java
@@ -2,7 +2,7 @@ package com.multitab.category.cate.application;
 
 
 
-import com.multitab.category.cate.dto.in.BottomCategoryRequestDto;
+
 import com.multitab.category.cate.dto.in.MiddleCategoryRequestDto;
 import com.multitab.category.cate.dto.in.TopCategoryRequestDto;
 import com.multitab.category.cate.dto.out.*;
@@ -19,11 +19,9 @@ public interface CategoryService {
     // 중 카테고리 생성
     void createMiddleCategory(MiddleCategoryRequestDto middleCategoryRequestDto);
     // 하 카테고리 생성
-    void createBottomCategory(BottomCategoryRequestDto bottomCategoryRequestDto);
 
     void updateTopCategory(TopCategoryRequestDto topCategoryRequestDto);
     void updateMiddleCategory(MiddleCategoryRequestDto middleCategoryRequestDto);
-    void updateBottomCategory(BottomCategoryRequestDto bottomCategoryRequestDto);
 
     void deleteTopCategory(Long topCategoryId);
     void deleteMiddleCategory(Long middleCategoryId);
@@ -33,12 +31,10 @@ public interface CategoryService {
     TopCategoryResponseDto getTopCategoryByCategoryCode(String topCategoryCode);
     MiddleCategoryResponseDto getMiddleCategory(Long middleCategoryId);
     MiddleCategoryResponseDto getMiddleCategoryByCategoryCode(String middleCategoryCode);
-    BottomCategoryResponseDto getBottomCategory(Long bottomCategoryId);
-    BottomCategoryResponseDto getBottomCategoryByCategoryCode(String bottomCategoryCode);
+
 
     List<TopCategoryResponseDto> getTopCategories();
     List<MiddleCategoryResponseDto> getMiddleCategories(String topCategoryCode); // name -> code
-    List<BottomCategoryResponseDto> getBottomCategories(String middleCategoryCode);
 
     // 대 카테고리의 자식 카테고리(중) 조회
     List<ChildCategoryResponseDto> findChildCategoriesByTopCategory (String categoryCode);
