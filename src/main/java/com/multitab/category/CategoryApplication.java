da6c888269b80199ae0555366b82a684145548cf
kimdayday <kdh39278@gmail.com>
2024-11-06 16:19:34 +0900

    comment: PR 위한 주석 추가

--- src/main/java/com/multitab/category/CategoryApplication.java
+++ src/main/java/com/multitab/category/CategoryApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+// branch 안 파고 작업하고 있었어요...
+// 전체 확인 부탁드려요..............ㅎㅎ
 public class CategoryApplication {
 
     public static void main(String[] args) {
