f3168a57032516ef7c6abd883ed2a81d2929b89d
DaeHee Kim <96685431+everydayday@users.noreply.github.com>
2024-11-26 11:46:03 +0900

    fix: fix kafkaProducerConfig uri value for deploy

--- src/main/java/com/multitab/category/api/kafka/KafkaProducerConfig.java
+++ src/main/java/com/multitab/category/api/kafka/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,11 +18,13 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @EnableKafka
 public class KafkaProducerConfig {
 
+    @Value("${kafka.cluster.uri}")
+    private String kafkaClusterUri;
+
     @Bean
     public ProducerFactory<String, UpdateCategoryRequeestDto> categoryUpdateProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-            "localhost:29092,localhost:39092,localhost:49092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaClusterUri);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
